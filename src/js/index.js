ff8262ce1d01a7de0202baa2e9d3dc98604be5f4
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-03-18 20:57:50 +0200

    add reviews component

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../index.html';
 import '../sass/style.scss';
 import { Accordion } from './modules/Accordion';
 import videoplayer from './modules/videoplayer';
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Controller } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
@@ -23,3 +23,23 @@ const jobsSwiper = new Swiper('.jobs__slider', {
     spaceBetween: 65,
     slidesPerView: 'auto',
 });
+
+const authorsSwiper = new Swiper('.reviews__author', {
+    modules: [Navigation, Controller],
+    loop: true,
+    slidesPerView: 1,
+});
+
+const reviewsSwiper = new Swiper('.reviews__wrapper', {
+    modules: [Navigation, Controller],
+    loop: true,
+    navigation: {
+        nextEl: '.reviews__button--next',
+        prevEl: '.reviews__button--prev',
+    },
+    spaceBetween: 65,
+    slidesPerView: 1,
+});
+
+authorsSwiper.controller.control = reviewsSwiper;
+reviewsSwiper.controller.control = authorsSwiper;
