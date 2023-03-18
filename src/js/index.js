20903cc015ec81a8d8e36c97ac21f7f70c6abb25
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-03-18 17:49:08 +0200

    add jobs component

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,24 @@ import '../index.html';
 import '../sass/style.scss';
 import { Accordion } from './modules/Accordion';
 import videoplayer from './modules/videoplayer';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 new Accordion('.find__items', {
     transitionSpeed: 400,
     transitionFunction: 'ease',
     title: true,
+});
+
+const jobsSwiper = new Swiper('.jobs__slider', {
+    modules: [Navigation],
+    navigation: {
+        nextEl: '.jobs__button--next',
+        prevEl: '.jobs__button--prev',
+    },
+    loop: true,
+    spaceBetween: 65,
+    slidesPerView: 'auto',
 });
