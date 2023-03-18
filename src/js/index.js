75916c69af16a16c7e50dfbf51abe264c6cef694
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-03-18 14:31:08 +0200

    add find component

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 import '../index.html';
 import '../sass/style.scss';
+import { Accordion } from './modules/Accordion';
+import videoplayer from './modules/videoplayer';
+
+new Accordion('.find__items', {
+    transitionSpeed: 400,
+    transitionFunction: 'ease',
+    title: true,
+});
