cb51f172d4209a065a010e124cdb0e8a8259df32
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-03-24 19:01:32 +0200

    add onClickOutsideHeaderHandler

--- src/js/modules/burgerMenu.js
+++ src/js/modules/burgerMenu.js
@@ -6,15 +6,32 @@ export default (function () {
     const body = document.body;
 
     burgerMenu.addEventListener('click', burgerOnClickHandler);
-    
+
+    document.addEventListener('click', onClickOutsideHeaderHandler);
+    document.addEventListener('touchstart', onClickOutsideHeaderHandler);
+
     function burgerOnClickHandler(event) {
         if (body.getAttribute(MOBILE_MENU_ATTR) !== null) {
-            burgerMenu.setAttribute('aria-expanded', false);
-            body.removeAttribute(MOBILE_MENU_ATTR);
+            closeMobileMenu();
         } else {
-            burgerMenu.setAttribute('aria-expanded', true);
-            body.setAttribute(MOBILE_MENU_ATTR, '');
+            openMobileMenu();
         }
+    }
+
+    function onClickOutsideHeaderHandler(event) {
+        if (body.getAttribute(MOBILE_MENU_ATTR) !== null && !event.target.closest('.header')) {
+            closeMobileMenu();
+        }
+    }
+    
+    function openMobileMenu() {
+        burgerMenu.setAttribute('aria-expanded', true);
+        body.setAttribute(MOBILE_MENU_ATTR, '');
+    }
+
+    function closeMobileMenu() {
+        burgerMenu.setAttribute('aria-expanded', false);
+        body.removeAttribute(MOBILE_MENU_ATTR);
     }
 
 })();
